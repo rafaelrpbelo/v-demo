b41598c70a12beafa61b443f0fd67a8a61700878
Rafael Belo <rafaelrpbelo@gmail.com>
2017-11-24 18:42:51 -0300

    Fetch data from firebase

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,46 @@
 <template>
   <div id='app' class='container'>
     <user-registration-form @submitted='handleUserRegistrationFormSubmit'></user-registration-form>
+    <hr />
+    <user-list :users='users' key='user'></user-list>
   </div>
 </template>
 
 <script>
   import UserRegistrationForm from './components/UserRegistrationForm.vue'
+  import UserList from './components/UserList.vue'
 
   export default {
+    data() {
+      return {
+        users: []
+      }
+    },
+    mounted() {
+      this.fetchUsers()
+    },
     methods: {
       handleUserRegistrationFormSubmit(data) {
         if (data == true) {
           alert('Form was submitted successfully =)')
+          this.fetchUsers()
         } else {
           alert('Something went wrong! =(')
         }
+      },
+      fetchUsers() {
+        this.$http.get('data.json').
+          then(response => response.json()).
+          then(data => this.users = data).
+          catch(e => {
+            alert('Occurred an error while fetching users')
+            console.log(e)
+          })
       }
     },
     components: {
-      userRegistrationForm: UserRegistrationForm
+      userRegistrationForm: UserRegistrationForm,
+      userList: UserList
     }
   }
 </script>
-
-<style lang='scss'>
-</style>
---
+++ src/components/UserList.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul class='list-group'>
+    <li class='list-group-item' v-for='user in users'>{{ user.username }} - {{ user.email }}</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    props: ['users']
+  }
+</script>
