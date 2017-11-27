28181cc9716325a0241f3748d6f0804f204dd5f7
Rafael Belo <rafaelrpbelo@gmail.com>
2017-11-27 16:19:42 -0300

    Add UserRegistrationForm to router

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,26 @@
 <template>
-  <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-3">V-Demo</h1>
-      <p class="lead">A set of examples written in Vue.js.</p>
+  <div>
+    <div class='row'>
+      <div class='col'>
+        <div class='jumbotron jumbotron-fluid'>
+          <div class='container'>
+            <h1 class='display-3'>V-Demo</h1>
+            <p class='lead'>A set of examples written in Vue.js.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class='container'>
+      <div class='row'>
+        <div class='col-lg-3 col-md-4 col-sm-6 card'>
+          <div class='card-body'>
+            <h4 class='card-title'>User Registration Form</h4>
+            <p class='card-text'>A simple user registration that persists data at Firebase.io</p>
+            <router-link to='/users/new' class='btn btn-primary'>Go to example</router-link>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import VueRouter from 'vue-router'
 import Home from './components/Home.vue'
+import UserRegistrationForm from './components/UserRegistrationForm.vue'
 
 export const routes = [
-  { path: '', component: Home }
+  { path: '', component: Home },
+  { path: '/users/new', component: UserRegistrationForm }
 ]
 
 export const router = new VueRouter({ routes })
